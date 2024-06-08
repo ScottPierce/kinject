e0d47996c47953da10d2226a05a083a996e6e738
Scott Pierce <871169+ScottPierce@users.noreply.github.com>
2024-06-07 22:43:39 -0400

    maven release

--- kinject/build.gradle.kts
+++ kinject/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.gradleMavenPublish)
 }
 
 kotlin {
@@ -20,6 +23,50 @@ kotlin {
     sourceSets {
         commonTest.dependencies {
             implementation(kotlin("test"))
+        }
+    }
+}
+
+val ref = System.getenv()["GITHUB_REF"]
+if (System.getenv()["GITHUB_REF_TYPE"] == "tag" && ref?.startsWith("refs/tags/v") == true) {
+    val version = ref.removePrefix("refs/tags/v")
+    println("Releasing Version: $version")
+
+    mavenPublishing {
+        publishToMavenCentral(SonatypeHost.S01, automaticRelease = true)
+
+        signAllPublications()
+
+        coordinates(
+            groupId = "io.github.scottpierce",
+            artifactId = "kinject",
+            version = version,
+        )
+
+        pom {
+            name.set("Kinject")
+            description.set("A simple Kotlin Multiplatform dependency injection library.")
+            inceptionYear.set("2024")
+            url.set("https://github.com/ScottPierce/kinject")
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+            developers {
+                developer {
+                    id.set("ScottPierce")
+                    name.set("Scott Pierce")
+                    url.set("https://github.com/ScottPierce")
+                }
+            }
+            scm {
+                url.set("https://github.com/ScottPierce/kinject")
+                connection.set("scm:git:git://github.com/ScottPierce/kinject.git")
+                developerConnection.set("scm:git:ssh://git@github.com/ScottPierce/kinject.git")
+            }
         }
     }
 }
