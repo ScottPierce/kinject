4a4baa37bce1c46cee7ed9ff2ca9043ed27b3470
Scott Pierce <871169+ScottPierce@users.noreply.github.com>
2024-07-06 16:55:47 -0400

    change function name from getViewModel to injectViewModel.

--- kinject-compose-viewmodel/src/main/kotlin/kinject/compose/viewmodel/GetViewModel.kt
+++ kinject-compose-viewmodel/src/main/kotlin/kinject/compose/viewmodel/GetViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 import kinject.compose.LocalKinjectContext
 
 @Composable
-inline fun <reified VM : ViewModel> getViewModel(): VM {
+inline fun <reified VM : ViewModel> injectViewModel(): VM {
     val vmClass = VM::class
     val kinjectContext = LocalKinjectContext.current
     val viewModelProvider = kinjectContext.viewModelProvider()
