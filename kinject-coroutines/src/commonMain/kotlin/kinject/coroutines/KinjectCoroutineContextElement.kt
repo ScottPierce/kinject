5604aaef7f080dad3c0d07702f8b211daa0e118c
Scott Pierce <871169+ScottPierce@users.noreply.github.com>
2024-07-06 13:41:00 -0400

    add documentation for kinject coroutines

--- kinject-coroutines/src/commonMain/kotlin/kinject/coroutines/KinjectCoroutineContextElement.kt
+++ kinject-coroutines/src/commonMain/kotlin/kinject/coroutines/KinjectCoroutineContextElement.kt
@@ -4,6 +4,7 @@ import kinject.ObjectGraph
 import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.coroutineContext
+import kinject.KinjectException
 
 class KinjectCoroutineContextElement(
     val objectGraph: ObjectGraph
@@ -11,18 +12,49 @@ class KinjectCoroutineContextElement(
     companion object Key : CoroutineContext.Key<KinjectCoroutineContextElement>
 }
 
+/**
+ * Takes the given [CoroutineContext] and combines it with a [KinjectCoroutineContextElement].
+ */
+operator fun CoroutineContext.plus(objectGraph: ObjectGraph): CoroutineContext =
+    this.plus(KinjectCoroutineContextElement(objectGraph))
+
+/**
+ * Retrieve the closest [ObjectGraph] from the current [CoroutineContext], or returns null if one
+ * can't be found.
+ */
 fun CoroutineContext.objectGraphOrNull(): ObjectGraph? {
     return this[KinjectCoroutineContextElement]?.objectGraph
 }
 
+/**
+ * Retrieve the closest [ObjectGraph] from the given [CoroutineContext].
+ *
+ * @throws [KinjectException] if the [ObjectGraph] can't be retrieved because there is no
+ *  [KinjectCoroutineContextElement] in the current [CoroutineContext].
+ */
 fun CoroutineContext.objectGraph(): ObjectGraph {
     return this[KinjectCoroutineContextElement]?.objectGraph
-        ?: error("Can't find 'KinjectCoroutineContextElement' in the current CoroutineContext.")
+        ?: throw KinjectException("Can't find 'KinjectCoroutineContextElement' in the current CoroutineContext.")
 }
 
-operator fun CoroutineContext.plus(objectGraph: ObjectGraph) =
-    this.plus(KinjectCoroutineContextElement(objectGraph))
-
+/**
+ * Retrieve the closest [ObjectGraph] from the current [CoroutineContext], or returns null if one
+ * can't be found.
+ */
 suspend fun objectGraphOrNull(): ObjectGraph? = coroutineContext.objectGraphOrNull()
 
+/**
+ * Retrieve the closest [ObjectGraph] from the current [CoroutineContext].
+ *
+ * @throws [KinjectException] if the [ObjectGraph] can't be retrieved because there is no
+ *  [KinjectCoroutineContextElement] in the current [CoroutineContext].
+ */
 suspend fun objectGraph(): ObjectGraph = coroutineContext.objectGraph()
+
+/**
+ * Lazily inject a dependency from the closest [ObjectGraph] in the current [CoroutineContext].
+ */
+suspend inline fun <reified T : Any> inject(): Lazy<T> {
+    val objectGraph = objectGraph()
+    return lazy { objectGraph.get(T::class) }
+}
